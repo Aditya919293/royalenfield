232e96a6f40f1671d8d24334d81cf101509582a0
Aditya Kini <108882371+Aditya919293@users.noreply.github.com>
2023-09-06 10:45:02 +0530

    Add files via upload

---
+++ re.js
@@ -0,0 +1,191 @@
+//motorcycles
+var tip =document.getElementById("motorcycles");
+var img2=document.getElementById("imagebody2");
+var mbtn=document.getElementById("motorbtn");
+function a(){
+    tip.src="./images/continental_gt_650_motorcycle.avif";
+    img2.style.top= "50%";
+    img2.style.left="5%";
+    mbtn.style.backgroundColor="#da291c";
+    mbtn.style.color="white";
+    mbtn.style.border="none";
+}
+
+function b(){
+    tip.src="./images/super-meteor-650.avif";
+    img2.style.left="80%";
+    img2.style.top= "50%";
+    mbtn.style.backgroundColor="#da291c";
+    mbtn.style.color="white";
+    mbtn.style.border="none";
+}
+
+function c(){
+    tip.src="./images/hunter-350-motorcycle-home.avif";
+    img2.style.top= "50%";
+    img2.style.left="5%";
+    mbtn.style.backgroundColor="transparent";
+    mbtn.style.color="white";
+    mbtn.style.border="2px solid white";
+}
+
+function d(){
+    tip.src="./images/scram.avif";
+    img2.style.left="72%";
+    img2.style.top= "80%";
+    mbtn.style.backgroundColor="#da291c";
+    mbtn.style.color="white";
+    mbtn.style.border="none";
+}
+
+function e(){
+    tip.src="./images/classic-350-motorcycle-section.avif";
+    img2.style.top= "50%";
+    img2.style.left="5%";
+    mbtn.style.backgroundColor="transparent";
+    mbtn.style.color="white";
+    mbtn.style.border="2px solid white";
+}
+
+function f(){
+    tip.src="./images/meteor350.avif";
+    img2.style.top= "58%";
+    img2.style.left="5%";
+    mbtn.style.backgroundColor="white";
+    mbtn.style.color="black";
+    mbtn.style.border="none";
+}
+
+function g(){
+    tip.src="./images/sleethblack-banner-des.avif";
+    img2.style.left="72%";
+    img2.style.top= "15%";
+    mbtn.style.backgroundColor="#da291c";
+    mbtn.style.color="white";
+    mbtn.style.border="none";
+}
+
+function h(){
+    tip.src="./images/bullet350.avif";
+    img2.style.top= "50%";
+    img2.style.left="5%";
+    mbtn.style.backgroundColor="transparent";
+    mbtn.style.color="white";
+    mbtn.style.border="2px solid white";
+}
+
+//rides
+var ride =document.getElementById("rides");
+function a1(){
+    ride.src="./images/motoverse2023.jfif";
+}
+
+function b1(){
+    ride.src="./images/trackschool.jfif";
+}
+
+function c1(){
+    ride.src="./images/himalayan-adventure-changthang.jpeg";
+}
+
+function d1(){
+    ride.src="./images/gtcup.jpeg";
+}
+
+function e1(){
+    ride.src="./images/himalayan_odyssey_2023.jpeg";
+}
+
+function f1(){
+    ride.src="./images/leh-rider-support.jpeg";
+}
+
+//apparel
+var apparel = document.getElementById("apparel");
+var img4=document.getElementById("imagebody4");
+var appbtn=document.getElementById("appbtn");
+
+function a2(){
+    apparel.src="./images/supermeteor650.jpeg";
+    img4.style.top="48%";
+    img4.style.left="5%";
+    appbtn.style.border="none";
+    appbtn.style.color="white";
+    appbtn.style.backgroundColor="#da291c";
+    appbtn.innerText="Know More";
+}
+
+function b2(){
+    apparel.src="./images/classiccollectable.jpeg";
+    img4.style.top="80%";
+    img4.style.left="70%";
+    appbtn.style.border="2px solid white";
+    appbtn.style.color="white";
+    appbtn.style.backgroundColor="transparent";
+    appbtn.innerText="Know More";
+}
+
+function c2(){
+    apparel.src="./images/royalenfield x alpinestar.jpeg.avif";
+    img4.style.top="80%";
+    img4.style.left="44%";
+    appbtn.style.border="2px solid white";
+    appbtn.style.color="white";
+    appbtn.style.backgroundColor="transparent";
+    appbtn.innerText="Shop Now";
+}
+
+function d2(){
+    apparel.src="./images/royal-enfield-tcx.jpeg";
+    img4.style.top="80%";
+    img4.style.left="5%";
+    appbtn.style.border="2px solid white";
+    appbtn.style.color="white";
+    appbtn.style.backgroundColor="transparent";
+    appbtn.innerText="Shop Now";
+}
+
+function e2(){
+    apparel.src="./images/miy-riding-jacket.jpeg";
+    img4.style.top="48%";
+    img4.style.left="5%";
+    appbtn.style.border="2px solid white";
+    appbtn.style.color="white";
+    appbtn.style.backgroundColor="transparent";
+    appbtn.innerText="Shop Now";
+
+}
+
+//Accessories
+var acce = document.getElementById("Accessories");
+function a3(){
+    acce.src="./images/makeityours.jpeg";
+    accbtn.style.backgroundColor="transparent";
+    accbtn.style.border="2px solid white";
+    accbtn.style.color="white";
+    img5.style.left="8%";
+}
+
+var accbtn = document.getElementById("accbtn");
+var img5 = document.getElementById("imagebody5");
+
+
+//locate
+var locate = document.getElementById("locate");
+function a4(){
+    locate.src="./images/service-centres.jpeg";
+    loch1.innerHTML="For Everything That Your "+"<br>"+"Motorcycle May Need";
+    locbtn.innerHTML="Find a Service Center";
+}
+
+function b4(){
+    locate.src="./images/genuine-parts.jpeg";
+    loch1.innerHTML="Keep it Real With Genuine Spare"+"<br>"+"Parts";
+    locbtn.innerHTML="Find a Distributor";
+}
+
+var loch1 = document.getElementById("loch1");
+var locbtn = document.getElementById("locbtn");
+
+
+
